99506e4ac4bd218e606992abe455aa23ee9291f5
Luciano Denio <lucianodenio@gmail.com>
2022-09-27 19:49:05 -0300

    First commit

---
+++ src/entities/Bill.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class Bill {
+	public char gender;
+	public int beer;
+	public int barbecue;
+	public int softDrink;
+	
+	public double cover() {
+		if (feeding( ) > 30) {
+			return  0;
+		}
+		else return 4;
+	}
+	
+	public double feeding() {
+		return beer * 5.0 + softDrink * 3.0 + barbecue * 7.0 ;
+	}
+	
+	public double ticket() {
+		if (gender == 'F') {
+			return 8.00;	
+		}
+		else return 10.0;	
+	}
+	
+	public double total() {
+		return cover() + feeding() + ticket();
+	}
+	
+}
